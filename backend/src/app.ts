986c38c6bbc76852c8bf9a4c2310b0fa58f8b8e0
paulaji <paulajiparayil123@gmail.com>
2023-06-13 15:59:17 +0530

    added the update notes feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
@@ -12,14 +13,18 @@ app.use(express.json());
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("404 not found!"));
+  next(createHttpError(404, "Status 404 - Not Found!"));
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
   let errorMessage = "We will be up and running in a while!";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 export default app;
